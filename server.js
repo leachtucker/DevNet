2df9b3c583aaac495354e281accac69d53d2eee8
Tucker <leachtucker@gmail.com>
2021-08-28 09:12:12 -0700

    Implements express routers for our needed resources

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express();
 // Configure & Connect DB
 connectDB();
 
+// Define Routers
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 app.get('/', (req, res) => {
   res.send('API running');
 });
