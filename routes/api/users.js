44117cafb075a81c8dd7576caf5318fa768c1b66
Tucker <leachtucker@gmail.com>
2021-08-28 10:51:00 -0700

    Implements POST method on user resource

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,56 @@
 const router = require('express').Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
-// @route   GET api/users
-// @desc    Test route
+const validate = require('../../middleware/validate');
+const errorHandler = require('../../middleware/errorHandler');
+
+const User = require('../../models/User');
+
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => {
-  res.send('User route');
+router.post('/', validate('registerUser'), async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    // Check for existing user
+    let user = await User.findOne({ email });
+
+    if (user) {
+      res.status(400).json({ errors: [{ msg: 'User already exists' }] })
+    }
+
+    // Get users gravatar--from email
+    const avatar = gravatar.url(email, {
+      s: '200',
+      r: 'pg',
+      d: 'mm'
+    });
+
+    // Assign new User object with the given properties
+    user = new User({
+      name,
+      email,
+      avatar,
+      password
+    });
+
+    // Encrypt password & Update User object
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    // Save user to db
+    await user.save();
+
+    // Return JWT
+    res.status(200).send("User registered");
+
+  } catch {
+    console.error(err.message);
+    res.status(500).send('Internal server error')
+  }
 });
 
 module.exports = router;
