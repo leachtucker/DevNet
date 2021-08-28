d57c2c453d92814472c128984e44285c51d99630
Tucker <leachtucker@gmail.com>
2021-08-28 11:38:33 -0700

    Implements the auth middleware on an endpoint

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
+const authMiddleware = require('../../middleware/auth');
+const User = require('../../models/User');
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => {
-  res.send('Auth route');
+router.get('/', authMiddleware(), async (req, res) => {
+  try {
+    console.log(req.user);
+    const user = await User.findById(req.user.id).select('-password');
+
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
