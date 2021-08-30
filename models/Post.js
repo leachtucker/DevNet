022894443a7b537c6d5c762e9ccb00399bfee426
Tucker <leachtucker@gmail.com>
2021-08-30 14:18:48 -0700

    Implements schema for Post resource

---
+++ models/Post.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ]
+});
+
+module.exports = model('post', PostSchema);
