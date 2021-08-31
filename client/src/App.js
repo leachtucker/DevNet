77c307c718d4a5a701179ec1cf98c9045facd171
Tucker <15221354+leachtucker@users.noreply.github.com>
2021-08-31 15:01:51 -0700

    Implements Routes for each new component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 import './App.css';
 
@@ -12,6 +14,12 @@ function App() {
       <>
         <Navbar />
         <Route exact path="/" component={ Landing } />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={ Register } />
+            <Route exact path="/login" component={ Login } />
+          </Switch>
+        </section>
       </>
     </Router>
   );
