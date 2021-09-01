69a32abb598707ab2543a0360e5cc64d1d56f824
Tucker <15221354+leachtucker@users.noreply.github.com>
2021-09-01 09:29:31 -0700

    Implements alerts

---
+++ client/src/components/layout/Alert.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+import { removeAlert } from '../../actions/alert';
+
+
+const Alert = props => {
+  return (
+    <>
+      {(props.alerts !== null && props.alerts.length > 0) &&
+        props.alerts.map(alert => {
+          return <div key={alert.id} className={`alert alert-${alert.alertType}`}>{alert.msg}</div>
+        })
+      }
+    </>
+  )
+}
+
+Alert.propTypes = {
+  alerts: PropTypes.array.isRequired,
+}
+
+const mapStateToProps = state => ({
+  alerts: state.alert
+});
+
+export default connect(mapStateToProps)(Alert)
