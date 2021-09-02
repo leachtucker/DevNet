e481e52cecfd9125d5dbf57d0d6eea6b645e8309
Tucker <15221354+leachtucker@users.noreply.github.com>
2021-09-02 13:20:30 -0700

    Implements private route for Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -40,7 +41,7 @@ function App() {
             <Switch>
               <Route exact path="/register" component={ Register } />
               <Route exact path="/login" component={ Login } />
-              <Route exact path="/dashboard" component={ Dashboard } />
+              <PrivateRoute exact path="/dashboard" component={ Dashboard } />
             </Switch>
           </section>
         </>
