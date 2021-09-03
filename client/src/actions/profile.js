4df66254fb83bf528396e0478d05ad0377b58798
Tucker <15221354+leachtucker@users.noreply.github.com>
2021-09-03 09:52:51 -0700

    Creates a redux action to create/update profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import { setAlert } from './alert';
 
 import { GET_PROFILE, PROFILE_ERROR } from './types';
 
+// Gets the current user's profile
 export const getCurrentProfile = () => async (dispatch) => {
   try {
     const res = await axios.get('/api/profile/me');
@@ -13,6 +15,40 @@ export const getCurrentProfile = () => async (dispatch) => {
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }
     });
-    // dispatch({ type: PROFILE_ERROR, payload: { msg: err.response.data.msg, status: err.response.status } });
   }
 };
+
+// Create or update profile
+export const createProfile =
+  (profileFields, history, edit = false) =>
+  async (dispatch) => {
+    try {
+      const res = await axios.post('/api/profile', profileFields);
+
+      // Dispatch
+      dispatch({ type: GET_PROFILE, payload: res.data });
+
+      // Send an alert!
+      dispatch(
+        setAlert(edit ? 'Profile updated!' : 'Profile created!', 'success')
+      );
+
+      // Redirect the user if profile was created--not updated
+      if (!edit) {
+        history.push('/dashboard');
+      }
+    } catch (err) {
+      console.error(err);
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+
+      const validationErrors = err.response.data.errors;
+      if (validationErrors) {
+        validationErrors.forEach((error) =>
+          dispatch(setAlert(error.msg, 'danger'))
+        );
+      }
+    }
+  };
