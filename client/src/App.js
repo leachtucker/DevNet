1a6db6ab85455ba1d9c569d497b6673f2383d47b
Tucker <15221354+leachtucker@users.noreply.github.com>
2021-09-03 09:17:48 -0700

    Creates & Implements Create Profile Form on private route

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 
 // Redux //
 import { Provider } from 'react-redux';
@@ -42,6 +43,11 @@ function App() {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </>
