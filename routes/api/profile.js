29af3a2a84585cac57a4bbbe447b674ea885b028
Tucker <15221354+leachtucker@users.noreply.github.com>
2021-09-03 08:17:23 -0700

    Fixes a status code for profile endpoint

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -16,7 +16,7 @@ router.get('/me', auth(), async (req, res) => {
     const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
 
     if (!profile) {
-      return res.status(400).json({ msg: "There is no profile for this user" });
+      return res.status(404).json({ msg: "There is no profile for this user" });
     }
 
     res.json(profile);
