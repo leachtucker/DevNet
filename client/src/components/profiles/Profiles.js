a73ef52aa9e10186269a2d10be03630795717a96
Tucker <15221354+leachtucker@users.noreply.github.com>
2021-09-06 14:58:15 -0700

    Implements the ProfileItem component to present each profiles information

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -3,28 +3,44 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import Spinner from '../layout/Spinner';
+import ProfileItem from './ProfileItem';
 
 import { getAllProfiles } from '../../actions/profile';
+import profile from '../../reducers/profile';
 
-const Profiles = ({ profiles, getAllProfiles }) => {
+const Profiles = ({ profiles, loading, getAllProfiles }) => {
   useEffect(() => {
     getAllProfiles();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (!profiles.length) {
+  if (loading) {
     return <Spinner />;
   }
 
-  return <div></div>;
+  return (
+    <>
+      <h1 className="large text-primary">Developers</h1>
+      <p className="lead">Browse and connect with fellow devs</p>
+      <div className="profiles">
+        {!!profiles.length === true ? (
+          profiles.map((prof) => <ProfileItem key={prof._id} profile={prof} />)
+        ) : (
+          <h4>There are no profiles yet</h4>
+        )}
+      </div>
+    </>
+  );
 };
 
 Profiles.propTypes = {
-  profiles: PropTypes.array.isRequired
+  profiles: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = (state) => ({
-  profiles: state.profile.profiles
+  profiles: state.profile.profiles,
+  loading: state.profile.loading
 });
 
 export default connect(mapStateToProps, { getAllProfiles })(Profiles);
