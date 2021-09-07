ae611144cd9b66f0c72864953ee6f4be9e1e9494
Tucker <15221354+leachtucker@users.noreply.github.com>
2021-09-07 10:12:09 -0700

    Removes unused import

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router';
 
 import { createProfile } from '../../actions/profile';
