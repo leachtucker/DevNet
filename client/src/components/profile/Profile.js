45137b16b84f92e08b9c4dc1e226c9df8922362a
Tucker <15221354+leachtucker@users.noreply.github.com>
2021-09-07 12:19:56 -0700

    Implements newly created component to display educations

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,8 +7,10 @@ import { useHistory } from 'react-router';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
 
 import { getProfileByID } from '../../actions/profile';
+import ProfileEducation from './ProfileEducation';
 
 const Profile = ({ match, profile, loading, auth, getProfileByID }) => {
   const history = useHistory();
@@ -34,6 +36,34 @@ const Profile = ({ match, profile, loading, auth, getProfileByID }) => {
       <div className="profile-grid my-1">
         <ProfileTop profile={profile} />
         <ProfileAbout profile={profile} />
+        {/* Experience List */}
+        <div className="profile-exp bg-white p-2">
+          <h2 className="text-primary">Experience</h2>
+          {!!profile.experience.length ? (
+            <>
+              {profile.experience.map((exp) => (
+                <ProfileExperience key={exp._id} experience={exp} />
+              ))}
+            </>
+          ) : (
+            <h4>No experience credentials</h4>
+          )}
+        </div>
+        {/* Education List */}
+        <div className="profile-edu bg-white p-2">
+          <h2 className="text-primary">Education</h2>
+          {!!profile.education.length ? (
+            <>
+              {profile.education.map((edu) => (
+                <ProfileEducation key={edu._id} education={edu} />
+              ))}
+            </>
+          ) : (
+            <>
+              <h4>No education credentials</h4>
+            </>
+          )}
+        </div>
       </div>
     </>
   );
