d0f1f768481c63eb3d3b11719d27c97b50ee72a8
Tucker <15221354+leachtucker@users.noreply.github.com>
2021-09-07 12:19:40 -0700

    Creates a component for presenting individual educations on the detailed profile view

---
+++ client/src/components/profile/ProfileEducation.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+
+const ProfileEducation = ({ education }) => {
+  return (
+    <div>
+      <h3>{education.school}</h3>
+      <p>
+        <Moment format="YYYY/MM/DD">{education.from}</Moment> -{' '}
+        {!education.to ? (
+          <span>Current</span>
+        ) : (
+          <Moment format="YYYY/MM/DD">{education.to}</Moment>
+        )}
+      </p>
+      <p>
+        <strong>Degree: </strong>
+        {education.degree}
+      </p>
+      <p>
+        <strong>Field Of Study: </strong>
+        {education.fieldofstudy}
+      </p>
+      {education.description && (
+        <p>
+          <strong>Description: </strong>
+          {education.description}
+        </p>
+      )}
+    </div>
+  );
+};
+
+ProfileEducation.propTypes = {
+  education: PropTypes.object.isRequired
+};
+
+export default ProfileEducation;
