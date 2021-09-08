7fb0c4a0ee9e4f91fa1e5b139ed0c23c886b44f8
Tucker <15221354+leachtucker@users.noreply.github.com>
2021-09-07 19:59:18 -0700

    Implements the newly created Posts component on a secured route

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 
 // Redux //
 import { Provider } from 'react-redux';
@@ -51,6 +52,7 @@ function App() {
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
               <Route exact path="/profile/:id" component={Profile} />
+              <PrivateRoute exact path="/posts" component={Posts} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
