88ba06a94d856a47776893be523789bd68123058
Tucker <15221354+leachtucker@users.noreply.github.com>
2021-09-09 11:33:09 -0700

    Prepares for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -15,9 +16,14 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-app.get('/', (req, res) => {
-  res.send('API running');
-});
+// Serve statis assests in prod
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Listens on the port from env vars--if none, run on 5000
 const PORT = process.env.PORT || 5000;
