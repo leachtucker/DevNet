80968744c128932c29f115d3952d70dc71cec540
Tucker <15221354+leachtucker@users.noreply.github.com>
2021-09-08 19:25:06 -0700

    Renders loading spinner if the post has not yet been fetched

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,46 +4,30 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { getPostByID } from '../../actions/post';
-import { getProfileByID, getAllProfiles } from '../../actions/profile';
 import { useHistory } from 'react-router';
 
 import Spinner from '../layout/Spinner';
 import PostCommentForm from './PostCommentForm';
 
-const Post = ({
-  match,
-  post,
-  profile,
-  loading,
-  getPostByID,
-  getProfileByID,
-  getAllProfiles
-}) => {
+const Post = ({ match, post, loading, getPostByID }) => {
   const history = useHistory();
 
   useEffect(() => {
     getPostByID(match.params.id);
   }, [getPostByID, match.params.id]);
 
-  useEffect(() => {
-    getAllProfiles();
-    if (post) {
-      getProfileByID(post.user._id);
-    }
-  }, [getProfileByID, post]);
-
-  if (loading) return <Spinner />;
+  if (loading || post === null) return <Spinner />;
 
   return (
     <>
       <button className="btn" onClick={() => history.goBack()}>
         Go Back
       </button>
-      {post && profile && (
+      {post && (
         <>
           <div className="post bg-white p-1 my-1">
             <div>
-              <Link to={`/profile/${profile ? profile._id : ''}`}>
+              <Link to={`/profile/${post.user._id}`}>
                 <img
                   className="round-img"
                   src={post.user.avatar}
@@ -117,22 +101,15 @@ const Post = ({
 
 Post.propTypes = {
   match: PropTypes.object.isRequired,
-  user: PropTypes.object.isRequired,
+  user: PropTypes.object,
   post: PropTypes.object,
-  profile: PropTypes.object,
-  loading: PropTypes.bool.isRequired,
-  getAllProfiles: PropTypes.func.isRequired
+  loading: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = (state) => ({
   user: state.auth.user,
   post: state.post.post,
-  profile: state.profile,
   loading: state.post.loading
 });
 
-export default connect(mapStateToProps, {
-  getPostByID,
-  getProfileByID,
-  getAllProfiles
-})(Post);
+export default connect(mapStateToProps, { getPostByID })(Post);
